e7aabbec8d30511cc03f17dc6430fb2412058eb1
Juan Camilo <orjuela9@gmail.com>
2022-11-10 06:37:59 -0500

    Create newsletter subscription form

--- hotel-booking/src/App.js
+++ hotel-booking/src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import NewsletterForm from "./components/newsletter-form/index"
 
 function App() {
   return (
+    
     <div className="App">
+      <NewsletterForm/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
