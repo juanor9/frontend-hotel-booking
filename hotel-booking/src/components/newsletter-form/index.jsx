c346770c04c7a2771d80236f01beda4aac42490c
Juan Camilo <orjuela9@gmail.com>
2022-11-10 07:02:46 -0500

    Minor fixes

--- hotel-booking/src/components/newsletter-form/index.jsx
+++ hotel-booking/src/components/newsletter-form/index.jsx
@@ -1,17 +1,30 @@
 import React from "react";
 import "./style.css";
 
-function NewsletterForm (){
-    return(
-        <section id="newsletter-form" className="newsletter-form">
-            <h1 className="newsletter-form__header">Suscribe Our News</h1>
-            <p className="newsletter-form__paragraph">Suscribe and recieve our newsletters to follow the news about our fresh and fantastic products.</p>
-            <form action="" className="newsletter-form__form">
-                <input type="email" name="email" id="email" className="newsletter-form__input-email" placeholder="Enter Your Email"/>
-                <button type="submit" className="newsletter-form__button-subscribe">Subscribe</button>
-            </form>
-        </section>
-    )
+function NewsletterForm() {
+  return (
+    <section id="newsletter-form" className="newsletter-form">
+      <div>
+        <h1 className="newsletter-form__header">Suscribe Our News</h1>
+        <p className="newsletter-form__paragraph">
+          Suscribe and recieve our newsletters to follow the news about our
+          fresh and fantastic products.
+        </p>
+      </div>
+      <form action="" className="newsletter-form__form">
+        <input
+          type="email"
+          name="email"
+          id="email"
+          className="newsletter-form__input-email"
+          placeholder="Enter Your Email"
+        />
+        <button type="submit" className="newsletter-form__button-subscribe">
+          Subscribe
+        </button>
+      </form>
+    </section>
+  );
 }
 
-export default NewsletterForm
+export default NewsletterForm;
