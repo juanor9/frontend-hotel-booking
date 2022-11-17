26971c4f88c6a4919b134f1ad41f03ac13572c6c
Momezz <96633549+Momezz@users.noreply.github.com>
2022-11-17 18:57:03 -0500

    view-only news component (#29)

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import './componets/NewsList/StylesNews.css';
 import './componets/NavigationBar/NavigationBar.css';
 import NavigationBar from './componets/NavigationBar/NavigationBar';
 import OfferCard from './componets/OfferCard/OfferCard';
@@ -15,20 +16,25 @@ import RegisteredCards from './componets/RegisteredCards/RegisteredCards';
 import MasterCard from './componets/RegisteredCards/images/mastercard.png';
 import BookingHistory from './componets/BookingHistory/BookingHistory';
 import HotelMiami from './componets/BookingHistory/images/hotelMiami.jpg';
+import NewsList from './componets/NewsList/NewsList';
 
 const App = () => (
   <div className="App">
-    <SuccessfulPayment />
-    <OfferCard title="Food Order" text="Complimantary Breakfast. Use Code RICAFOOD" image={OfferImg} />
-    <NavigationBar />
-    <NewsletterForm />
-    <HotelCard hotelImg={HotelImg} name="The W South" place="Miami" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo distinctio, deserunt similique magnam ipsa commodi ea." reviews={26412} price={1300} finalPrice={1245} feature1="Swimming" feature2="Parking" />
-    <BookingConfirmationForm />
-    <PerfilMenu name="Mark Enderess" phone="+91 123 - 456 - 7890" email="mark.enderess@mail.com" imagePerfil={ImgPerfil} />
-    <UserInfo name="Mark Enderess" birthday="25/12/1990" gender="Female" address="549 Sulphur Springs Road" city="Downers Grove, IL" zip={60515} email="mark.enderess@mail.com" phone="+91 123 - 456 - 7890" password="************" />
-    <RegisterForm />
-    <RegisteredCards cardLogo={MasterCard} cardNumber="**** **** **** 1128" cardCompany="MasterCard" />
-    <BookingHistory hotelImg={HotelMiami} name="The W South" checkIn="25/11/2022" checkOut="27/11/2022" price={2300} />
+    <header className="App-header">
+      <NewsList />
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
   </div>
 );
 
