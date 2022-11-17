708337268e7dbac5b1fa831a44a69c4fb27a055f
Juan Camilo Orjuela <orjuela9@gmail.com>
2022-11-17 11:12:00 -0500

    Feature/hb 42 newsletter (#11)
    * Create register form for newsletter
    
    * Update file naming

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import NavigationBar from './componets/NavigationBar/NavigationBar';
 import OfferCard from './componets/OfferCard/OfferCard';
 import OfferImg from './componets/OfferCard/images/offerFood.jpg';
 import SuccessfulPayment from './componets/SuccessfulPayment/SuccessfulPayment';
+import NewsletterForm from './componets/NewsletterForm/NewsletterForm';
 
 const App = () => (
   <div className="App">
     <SuccessfulPayment />
     <OfferCard title="Food Order" text="Complimantary Breakfast. Use Code RICAFOOD" image={OfferImg} />
     <NavigationBar />
+    <NewsletterForm />
   </div>
 );
 
