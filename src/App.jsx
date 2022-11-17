7d855149b9639d89944e01b836677c22fae7385e
Momezz <96633549+Momezz@users.noreply.github.com>
2022-11-17 11:03:08 -0500

    navigation bar view (#13)

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import './componets/NavigationBar/NavigationBar.css';
+import NavigationBar from './componets/NavigationBar/NavigationBar';
 import OfferCard from './componets/OfferCard/OfferCard';
 import OfferImg from './componets/OfferCard/images/offerFood.jpg';
 import SuccessfulPayment from './componets/SuccessfulPayment/SuccessfulPayment';
@@ -6,6 +8,7 @@ const App = () => (
   <div className="App">
     <SuccessfulPayment />
     <OfferCard title="Food Order" text="Complimantary Breakfast. Use Code RICAFOOD" image={OfferImg} />
+    <NavigationBar />
   </div>
 );
 
