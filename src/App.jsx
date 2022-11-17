9c91280c3115c275a8642db431811672633815f5
dcquinche <90289781+dcquinche@users.noreply.github.com>
2022-11-17 11:24:27 -0500

    Solution to Hotel Card Component (#18)
    * Solution to Hotel Card Component
    
    * Add props and correction to media query

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import OfferCard from './componets/OfferCard/OfferCard';
 import OfferImg from './componets/OfferCard/images/offerFood.jpg';
 import SuccessfulPayment from './componets/SuccessfulPayment/SuccessfulPayment';
 import NewsletterForm from './componets/NewsletterForm/NewsletterForm';
+import HotelCard from './componets/HotelCard/HotelCard';
+import HotelImg from './componets/HotelCard/images/hotelMiami.jpg';
 
 const App = () => (
   <div className="App">
@@ -11,6 +13,7 @@ const App = () => (
     <OfferCard title="Food Order" text="Complimantary Breakfast. Use Code RICAFOOD" image={OfferImg} />
     <NavigationBar />
     <NewsletterForm />
+    <HotelCard hotelImg={HotelImg} name="The W South" place="Miami" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo distinctio, deserunt similique magnam ipsa commodi ea." reviews={26412} price={1300} finalPrice={1245} feature1="Swimming" feature2="Parking" />
   </div>
 );
 
