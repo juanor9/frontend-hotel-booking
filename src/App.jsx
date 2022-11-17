54ba6639535001b4223c7fba9a1ee33b28b7e95d
dcquinche <90289781+dcquinche@users.noreply.github.com>
2022-11-17 11:34:07 -0500

    Solution to User Information Component (#23)
    * Solution to User Information Component
    
    * Add props and correction to media query

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,10 @@ import SuccessfulPayment from './componets/SuccessfulPayment/SuccessfulPayment';
 import NewsletterForm from './componets/NewsletterForm/NewsletterForm';
 import HotelCard from './componets/HotelCard/HotelCard';
 import HotelImg from './componets/HotelCard/images/hotelMiami.jpg';
+import BookingConfirmationForm from './componets/BookingConfirmationForm/BookingConfirmationForm';
 import PerfilMenu from './componets/PerfilMenu/PerfilMenu';
 import ImgPerfil from './componets/PerfilMenu/images/perfilImage.jpg';
+import UserInfo from './componets/UserInfo/UserInfo';
 import RegisterForm from './componets/RegisterForm/RegisterForm';
 
 const App = () => (
@@ -17,7 +19,9 @@ const App = () => (
     <NavigationBar />
     <NewsletterForm />
     <HotelCard hotelImg={HotelImg} name="The W South" place="Miami" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo distinctio, deserunt similique magnam ipsa commodi ea." reviews={26412} price={1300} finalPrice={1245} feature1="Swimming" feature2="Parking" />
+    <BookingConfirmationForm />
     <PerfilMenu name="Mark Enderess" phone="+91 123 - 456 - 7890" email="mark.enderess@mail.com" imagePerfil={ImgPerfil} />
+    <UserInfo name="Mark Enderess" birthday="25/12/1990" gender="Female" address="549 Sulphur Springs Road" city="Downers Grove, IL" zip={60515} email="mark.enderess@mail.com" phone="+91 123 - 456 - 7890" password="************" />
     <RegisterForm />
   </div>
 );
