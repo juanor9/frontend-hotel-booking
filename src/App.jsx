4e24f5647859f9af28fafdd73d64ee7156fa0bf5
dcquinche <90289781+dcquinche@users.noreply.github.com>
2022-11-17 11:27:20 -0500

    Solution to Profile Menu Component (#19)
    * Solution to Profile Menu Component
    
    * Add props and correction to media query

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import SuccessfulPayment from './componets/SuccessfulPayment/SuccessfulPayment';
 import NewsletterForm from './componets/NewsletterForm/NewsletterForm';
 import HotelCard from './componets/HotelCard/HotelCard';
 import HotelImg from './componets/HotelCard/images/hotelMiami.jpg';
+import PerfilMenu from './componets/PerfilMenu/PerfilMenu';
+import ImgPerfil from './componets/PerfilMenu/images/perfilImage.jpg';
 
 const App = () => (
   <div className="App">
@@ -14,6 +16,7 @@ const App = () => (
     <NavigationBar />
     <NewsletterForm />
     <HotelCard hotelImg={HotelImg} name="The W South" place="Miami" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo distinctio, deserunt similique magnam ipsa commodi ea." reviews={26412} price={1300} finalPrice={1245} feature1="Swimming" feature2="Parking" />
+    <PerfilMenu name="Mark Enderess" phone="+91 123 - 456 - 7890" email="mark.enderess@mail.com" imagePerfil={ImgPerfil} />
   </div>
 );
 
