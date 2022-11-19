d413c509765eb5222dfe59ad4030f73acdb704b4
Juan Camilo Orjuela <orjuela9@gmail.com>
2022-11-19 12:37:41 -0500

    Create explainer component (#30)

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,31 @@
-// import './componets/NewsList/StylesNews.css';
-// import './componets/NavigationBar/NavigationBar.css';
-// import NavigationBar from './componets/NavigationBar/NavigationBar';
-// import OfferCard from './componets/OfferCard/OfferCard';
-// import OfferImg from './componets/OfferCard/images/offerFood.jpg';
-// import SuccessfulPayment from './componets/SuccessfulPayment/SuccessfulPayment';
-// import NewsletterForm from './componets/NewsletterForm/NewsletterForm';
-// import HotelCard from './componets/HotelCard/HotelCard';
-// import HotelImg from './componets/HotelCard/images/hotelMiami.jpg';
-// import PerfilMenu from './componets/PerfilMenu/PerfilMenu';
-// import ImgPerfil from './componets/PerfilMenu/images/perfilImage.jpg';
-// import UserInfo from './componets/UserInfo/UserInfo';
-// import RegisterForm from './componets/RegisterForm/RegisterForm';
-// import RegisteredCards from './componets/RegisteredCards/RegisteredCards';
-// import MasterCard from './componets/RegisteredCards/images/mastercard.png';
-// import BookingHistory from './componets/BookingHistory/BookingHistory';
-// import HotelMiami from './componets/BookingHistory/images/hotelMiami.jpg';
-// import NewsList from './componets/NewsList/NewsList';
-// import HotelRooms from './componets/HotelRooms/HotelRooms';
+import Explainer from './componets/Explainer/Explainer';
+import './componets/NewsList/StylesNews.css';
+import './componets/NavigationBar/NavigationBar.css';
+import NavigationBar from './componets/NavigationBar/NavigationBar';
+import OfferCard from './componets/OfferCard/OfferCard';
+import OfferImg from './componets/OfferCard/images/offerFood.jpg';
+import SuccessfulPayment from './componets/SuccessfulPayment/SuccessfulPayment';
+import NewsletterForm from './componets/NewsletterForm/NewsletterForm';
+import HotelCard from './componets/HotelCard/HotelCard';
+import HotelImg from './componets/HotelCard/images/hotelMiami.jpg';
+import PerfilMenu from './componets/PerfilMenu/PerfilMenu';
+import ImgPerfil from './componets/PerfilMenu/images/perfilImage.jpg';
+import UserInfo from './componets/UserInfo/UserInfo';
+import RegisterForm from './componets/RegisterForm/RegisterForm';
+import RegisteredCards from './componets/RegisteredCards/RegisteredCards';
+import MasterCard from './componets/RegisteredCards/images/mastercard.png';
+import BookingHistory from './componets/BookingHistory/BookingHistory';
+import HotelMiami from './componets/BookingHistory/images/hotelMiami.jpg';
+import NewsList from './componets/NewsList/NewsList';
+import HotelRooms from './componets/HotelRooms/HotelRooms';
 import HotelFilter from './componets/HotelFilter/HotelFilter';
+import BookingConfirmationForm from './componets/BookingConfirmationForm/BookingConfirmationForm'
 
 const App = () => (
   <div className="App">
+    <Explainer />
     <HotelFilter />
-    {/* <SuccessfulPayment />
+    <SuccessfulPayment />
     <OfferCard
       title="Food Order"
       text="Complimantary Breakfast. Use Code RICAFOOD"
@@ -67,7 +70,7 @@ const App = () => (
     <HotelRooms />
     <NewsList />
     <BookingHistory />
-    <HotelMiami /> */}
+    <HotelMiami />
   </div>
 );
 
