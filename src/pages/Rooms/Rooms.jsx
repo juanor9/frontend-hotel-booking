195cba4a5d30b1c57937a10c804d333edb72d92a
Juan Camilo Orjuela <orjuela9@gmail.com>
2022-11-20 11:40:08 -0500

    create general structure of hotel detail page

--- src/pages/Rooms/Rooms.jsx
+++ src/pages/Rooms/Rooms.jsx
@@ -1,6 +1,27 @@
+import './style.css';
+import NavigationBar from '../../components/NavigationBar/NavigationBar';
+import HotelRooms from '../../components/HotelRooms/HotelRooms';
+import BookingForm from '../../components/BookingForm/BookingForm';
+import Weather from '../../components/Weather/Weather';
+
 const Rooms = () => (
-  <div>
-    Rooms
+  <div className="hotel-details">
+    <header className="hotel-details__header">
+      <NavigationBar />
+    </header>
+    Hotel details
+    <div className="hotel-details__main-container">
+      <main>
+        <HotelRooms />
+      </main>
+      <aside>
+        <BookingForm />
+        Contact info component
+        <Weather />
+      </aside>
+    </div>
+
+    <footer>Page footer</footer>
   </div>
 );
 
