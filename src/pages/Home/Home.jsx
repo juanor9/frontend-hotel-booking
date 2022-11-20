bedef975fe5bf22dac645f173d425fbad1bac118
Julian Gomez <julgomez14@gmail.com>
2022-11-20 12:34:59 -0500

    I place components

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,20 @@
+import Footer from '../../components/footerInfo/Footer';
+import NavigationBar from '../../components/NavigationBar/NavigationBar';
+import HotelFilter from '../../components/HotelFilter/HotelFilter';
+import Explainer from '../../components/Explainer/Explainer';
+import OfferCard from '../../components/OfferCard/OfferCard';
+
 const Home = () => (
   <div>
-    Home
+    <NavigationBar />
+    <HotelFilter />
+    <OfferCard
+      title="ttttttttttttttttttttt"
+      text="reteret reter"
+      image="https://tse4.mm.bing.net/th?id=OIP.YvrLKP2jhEQqlcV0AtI2ngHaFc&pid=Api&P=0"
+    />
+    <Explainer />
+    <Footer />
   </div>
 );
 
