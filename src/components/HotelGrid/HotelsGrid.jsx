1267f711d20265f1f72bbb5e6ae0bf7a8a2edf3e
GDiana <dcquinche@gmail.com>
2022-11-21 06:52:18 -0500

    Solution to Hotels List Page

--- src/components/HotelGrid/HotelsGrid.jsx
+++ src/components/HotelGrid/HotelsGrid.jsx
@@ -1,13 +1,7 @@
-import React from 'react';
 import './StylesGrid.css';
 
 const HotelsGrid = () => (
   <section className="hotel__card">
-    <div className="hotel__grid">
-      <div className="component__card">
-        <p>HERE IS A COMPONENT</p>
-      </div>
-    </div>
     <div className="butons_card">
       <button type="submit">{'<<'}</button>
       <button type="submit">1</button>
