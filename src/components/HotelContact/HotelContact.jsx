7dc0f13e1214b7f9b3076590496ab049199e9cb6
Juan Camilo Orjuela <orjuela9@gmail.com>
2022-11-20 21:20:07 -0500

    Include props on component

--- src/components/HotelContact/HotelContact.jsx
+++ src/components/HotelContact/HotelContact.jsx
@@ -1,20 +1,47 @@
 import './style.css';
+import PropTypes from 'prop-types';
 
-const HotelContact = () => (
-  <section id="hotel-contact" className="hotel-contact">
-    <h3 className="hotel-contact__header">Contact Info</h3>
-    <div className="hotel-contact__contact-details">
-      <p className="hotel-contact__address">Address</p>
-      <p className="hotel-contact__phone">Phone number</p>
-      <p className="hotel-contact__email">E-mail</p>
-      <ul className="hotel-contact__social-media">
-        <li className="hotel-contact__social-media-item">Social Media</li>
-      </ul>
-      <hr className="hotel-contact__divider" />
-      <p className="hotel-contact__checkin">Check-in</p>
-      <p className="hotel-contact__checkout">Check-in</p>
-    </div>
+const HotelContact = (props) => {
+  const {
+    address,
+    phone,
+    email,
+    socialMedia,
+    checkin,
+    checkout,
+  } = props;
+  return (
+    <section id="hotel-contact" className="hotel-contact">
+      <h3 className="hotel-contact__header">Contact Info</h3>
+      <div className="hotel-contact__contact-details">
+        <p className="hotel-contact__address">{address}</p>
+        <p className="hotel-contact__phone">{phone}</p>
+        <p className="hotel-contact__email">{email}</p>
+        <ul className="hotel-contact__social-media">
+          <li className="hotel-contact__social-media-item">{socialMedia}</li>
+        </ul>
+        <hr className="hotel-contact__divider" />
+        <p className="hotel-contact__checkin">{checkin}</p>
+        <p className="hotel-contact__checkout">{checkout}</p>
+      </div>
 
-  </section>
-);
+    </section>
+  );
+};
+HotelContact.propTypes = {
+  address: PropTypes.string,
+  phone: PropTypes.string,
+  email: PropTypes.string,
+  socialMedia: PropTypes.arrayOf(PropTypes.string),
+  checkin: PropTypes.string,
+  checkout: PropTypes.string,
+};
+HotelContact.defaultProps = {
+  address: 'No address available',
+  phone: 'No phone number available',
+  email: 'No e-mail available',
+  socialMedia: [''],
+  checkin: 'No check-in hour available',
+  checkout: 'No check-out hour available',
+};
 export default HotelContact;
