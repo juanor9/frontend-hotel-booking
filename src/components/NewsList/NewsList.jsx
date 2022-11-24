e428b41ae69d55ffe5754657b4af66744b42b482
Julian Gomez <julgomez14@gmail.com>
2022-11-24 17:32:48 -0500

    change class name

--- src/components/NewsList/NewsList.jsx
+++ src/components/NewsList/NewsList.jsx
@@ -1,7 +1,7 @@
 import './styles.css';
 
 const NewsList = () => (
-  <article className="news__card">
+  <article className="news__list">
     <div className="news__card__image">
       <img className="news__card__img" src="https://tse4.mm.bing.net/th?id=OIP.hIom1U0HTOfsRfQ8X2cz0wHaFj&pid=Api&P=0" alt="news img" />
     </div>
@@ -9,7 +9,7 @@ const NewsList = () => (
       <h3 className="news__card__title">
         Recent News
       </h3>
-      <p>
+      <p className="news__card__paragraph">
         Lorem ipsum dolor Lorem ipsum dolor amet consectetur adipisicing elit adipisicing elit...
       </p>
     </div>
