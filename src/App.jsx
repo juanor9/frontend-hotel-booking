df28b66b857161ad56e322f5bf4959389555ee19
Juan Camilo Orjuela <orjuela9@gmail.com>
2022-11-25 10:52:57 -0500

    Create structure of users profile page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 import Hotels from './pages/Hotels/Hotels';
 import Rooms from './pages/Rooms/Rooms';
 import Home from './pages/Home/Home';
+import UserProfile from './pages/UserProfile/UserProfile';
 
 const App = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/hotels" element={<Hotels />} />
     <Route path="/hotel/:id" element={<Rooms />} />
+    <Route path="/profile" element={<UserProfile />} />
   </Routes>
 );
 
---
+++ src/pages/UserProfile/UserProfile.jsx
@@ -0,0 +1,14 @@
+import NavigationBar from '../../components/NavigationBar/NavigationBar';
+import Footer from '../../components/Footer/Footer';
+import PerfilMenu from '../../components/PerfilMenu/PerfilMenu';
+
+const UserProfile = () => (
+  <div className="hotel-details">
+    <NavigationBar />
+    holi
+    <PerfilMenu />
+    <Footer />
+  </div>
+);
+
+export default UserProfile;
