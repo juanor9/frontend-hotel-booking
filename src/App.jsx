5798dbde1f61757a2bcba0cdfdf571d44fefa279
Juan Camilo Orjuela <orjuela9@gmail.com>
2022-11-26 12:34:28 -0500

    Create user registration page (#43)
    * Create user registration page
    
    * Start form function
    
    * Create form functionality on json server
    
    * Turned eslint on

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 import Hotels from './pages/Hotels/Hotels';
 import Rooms from './pages/Rooms/Rooms';
 import Home from './pages/Home/Home';
+import UserRegistration from './pages/UserRegistration/UserRegistration';
 
 const App = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/hotels" element={<Hotels />} />
     <Route path="/hotel/:id" element={<Rooms />} />
+    <Route path="/register" element={<UserRegistration />} />
   </Routes>
 );
 
