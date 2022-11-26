b7d1cec9a1e1b5febdf23aeca30d1ceccb415367
Juan Camilo Orjuela <orjuela9@gmail.com>
2022-11-26 12:33:08 -0500

    Improve design

--- src/pages/UserProfile/UserProfile.jsx
+++ src/pages/UserProfile/UserProfile.jsx
@@ -1,12 +1,15 @@
 import NavigationBar from '../../components/NavigationBar/NavigationBar';
 import Footer from '../../components/Footer/Footer';
-import PerfilMenu from '../../components/PerfilMenu/PerfilMenu';
+import ProfileMenu from '../../components/ProfileMenu/ProfileMenu';
+import UserInfo from '../../components/UserInfo/UserInfo';
 
 const UserProfile = () => (
   <div className="hotel-details">
     <NavigationBar />
-    holi
-    <PerfilMenu />
+    <div className="hotel-details__container">
+      <ProfileMenu />
+      <UserInfo />
+    </div>
     <Footer />
   </div>
 );
