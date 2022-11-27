e5a834a3470832de80638f3598a1d3d7a7918adc
Momezz <96633549+Momezz@users.noreply.github.com>
2022-11-26 19:41:27 -0500

    Feature/hb 56 success (#44)
    * update remote changes
    
    * I create success page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import Success from './pages/Success/Success';
 import Hotels from './pages/Hotels/Hotels';
 import Rooms from './pages/Rooms/Rooms';
 import Home from './pages/Home/Home';
@@ -9,6 +10,7 @@ const App = () => (
     <Route path="/" element={<Home />} />
     <Route path="/hotels" element={<Hotels />} />
     <Route path="/hotel/:id" element={<Rooms />} />
+    <Route path="/success" element={<Success />} />
     <Route path="/register" element={<UserRegistration />} />
   </Routes>
 );
