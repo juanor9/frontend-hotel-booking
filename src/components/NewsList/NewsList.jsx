2b8399e0643c1eb334ebe0351c713887d116c64e
Momezz <96633549+Momezz@users.noreply.github.com>
2022-11-26 19:40:47 -0500

    Feature/hb 41 news list (#42)
    * view-only news component
    
    * change class name
    
    * I improve styles and bem-notation
    
    * Improved class names

--- src/components/NewsList/NewsList.jsx
+++ src/components/NewsList/NewsList.jsx
@@ -1,17 +1,16 @@
 import './styles.css';
 
 const NewsList = () => (
-
-  <article className="card__news">
-    <div className="card__image">
-      <img src="https://tse4.mm.bing.net/th?id=OIP.hIom1U0HTOfsRfQ8X2cz0wHaFj&pid=Api&P=0" alt="news img" />
+  <article className="news__list-cont">
+    <div className="news__card--image">
+      <img className="news__card--img" src="https://tse4.mm.bing.net/th?id=OIP.hIom1U0HTOfsRfQ8X2cz0wHaFj&pid=Api&P=0" alt="news img" />
     </div>
-    <div className="card__text">
-      <h3 className="card__title">
+    <div className="news__card--text">
+      <h3 className="news__card--title">
         Recent News
       </h3>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit adipisicing elit...
+      <p className="news__card--paragraph">
+        Lorem ipsum  amet adipisicingadipisicing elit...
       </p>
     </div>
   </article>
