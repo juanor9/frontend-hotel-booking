8dd83206e72ec06ba05abff5c352db4a26586dde
Juan Camilo Orjuela <orjuela9@gmail.com>
2022-11-28 07:53:44 -0500

    Feature/hb 5 user profile (#49)
    * Create structure of users profile page
    
    * Improve design
    
    * fixed bem notation
    
    * Fix styles
    
    * resolve conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Success from './pages/Success/Success';
 import Hotels from './pages/Hotels/Hotels';
 import Rooms from './pages/Rooms/Rooms';
 import Home from './pages/Home/Home';
+import UserProfile from './pages/UserProfile/UserProfile';
 import UserRegistration from './pages/UserRegistration/UserRegistration';
 import HotelsRegistration from './pages/HotelsRegistration/HotelsRegistration';
 
@@ -11,6 +12,7 @@ const App = () => (
     <Route path="/" element={<Home />} />
     <Route path="/hotels" element={<Hotels />} />
     <Route path="/hotel/:id" element={<Rooms />} />
+    <Route path="/profile" element={<UserProfile />} />
     <Route path="/success" element={<Success />} />
     <Route path="/register" element={<UserRegistration />} />
     <Route path="/hotels-registration" element={<HotelsRegistration />} />
