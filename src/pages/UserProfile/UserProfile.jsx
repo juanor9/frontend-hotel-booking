e91ac6d3d9d860fdd804df8d03ad651ed7c2ec87
Juan Camilo Orjuela <orjuela9@gmail.com>
2022-11-27 19:36:15 -0500

    Fix styles

--- src/pages/UserProfile/UserProfile.jsx
+++ src/pages/UserProfile/UserProfile.jsx
@@ -2,13 +2,22 @@ import NavigationBar from '../../components/NavigationBar/NavigationBar';
 import Footer from '../../components/Footer/Footer';
 import ProfileMenu from '../../components/ProfileMenu/ProfileMenu';
 import UserInfo from '../../components/UserInfo/UserInfo';
+import SliderNav from '../../components/SliderNav/SliderNav';
+import './style.css';
 
 const UserProfile = () => (
-  <div className="hotel-details">
-    <NavigationBar />
-    <div className="hotel-details__container">
-      <ProfileMenu />
-      <UserInfo />
+  <div className="profile">
+    <div className="profile__header">
+      <NavigationBar />
+      <SliderNav />
+    </div>
+    <div className="profile__container">
+      <div className="profile__profile-menu">
+        <ProfileMenu />
+      </div>
+      <div className="profile__user-info">
+        <UserInfo />
+      </div>
     </div>
     <Footer />
   </div>
