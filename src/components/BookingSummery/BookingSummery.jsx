55f5fd81e63ea54ddb378fc786bb77ae959ab787
Julian Gomez <julgomez14@gmail.com>
2022-12-16 14:06:44 -0500

    reservation page view

--- src/components/BookingSummery/BookingSummery.jsx
+++ src/components/BookingSummery/BookingSummery.jsx
@@ -1,13 +1,17 @@
+import PropTypes from 'prop-types';
 import './styles.css';
 
-const BookingSummery = () => (
+const BookingSummery = ({ reservationInformation }) => (
   <div className="summery__container">
     <h2>Booking Summery</h2>
     <div className="summary__view">
-      <img className="summery__image" src="https://tse4.mm.bing.net/th?id=OIP.h-R1e474we3Kw27WDmz-zwHaEK&pid=Api&P=0" alt="img-b" />
+      <img className="summery__image" src={reservationInformation.image} alt="img" />
       <div>
-        <h3>Sea View Hotel</h3>
-        <p>Mina Road, Bur Dubai, Dubai</p>
+        <h3>{reservationInformation.hotelDescription}</h3>
+        <p>{reservationInformation.nameHotel}
+          {reservationInformation.city}
+          {reservationInformation.country}
+        </p>
       </div>
     </div>
     <hr className="summery__hr" />
@@ -15,43 +19,50 @@ const BookingSummery = () => (
       <div className="summery__dates-item">
         <p>Check In</p>
         <span>
-          Tue, 18 Sep 2019
+          {reservationInformation.checkIn}
         </span>
       </div>
       <div className="summery__dates-item summery__left">
         <p>Check Out</p>
         <span>
-          Fri, 21 Sep 2019
+          {reservationInformation.checkOut}
         </span>
       </div>
       <div className="summery__dates-item">
         <p>Check In Time</p>
         <span>
-          2.00 Pm
+          {reservationInformation.checkInTime}
         </span>
       </div>
       <div className="summery__dates-item summery__left">
         <p>Check Out Time</p>
         <span>
-          12.00 Pm
+          {reservationInformation.checkOutTime}
         </span>
       </div>
     </div>
     <hr className="summery__hr" />
     <div className="summery__edit">
-      <p>2 Guest, 1 Deluxe Room, 3 Night </p>
+      <p>{reservationInformation.guestNumber},
+        {reservationInformation.typeOfRoom},
+        {reservationInformation.nightsNomber}
+      </p>
       <button className="summeery__btn" type="button">Edit</button>
     </div>
     <hr className="summery__hr" />
     <div>
       <h3>Payment Details</h3>
-      <p className="summery__details">Base Price <span>$2510</span></p>
-      <p className="summery__details">Promo Discount <span>-$250</span></p>
-      <p className="summery__details">Tax & ServiceFees <span>+$150</span></p>
+      <p className="summery__details">Base Price <span>{reservationInformation.basePrice}</span></p>
+      <p className="summery__details">Promo Discount <span>{reservationInformation.promoDiscount}</span></p>
+      <p className="summery__details">Tax & ServiceFees <span>{reservationInformation.taxes}</span></p>
       <hr className="summery__hr" />
       <p className="summery__details">Payable Amount <span className="summery__total">$2410</span></p>
     </div>
   </div>
 );
+
+BookingSummery.propTypes = {
+  reservationInformation: PropTypes.arrayOf.isRequired,
+};
 
 export default BookingSummery;
--- src/pages/Booking/Booking.jsx
+++ src/pages/Booking/Booking.jsx
@@ -4,26 +4,44 @@ import TravellerInfo from '../../components/TravellerInfo/TavellerInfo';
 import BookingSummery from '../../components/BookingSummery/BookingSummery';
 import './styles.css';
 
-const Booking = () => (
-  <>
-    <header className="booking__container">
-      <nav>
-        <NavigationBar />
-      </nav>
-      <div className="booking__nav">
-        <SliderNav />
-      </div>
-    </header>
-    <article className="booking__article">
-      <div className="booking__form booking__article--left">
-        <BookingSummery />
-      </div>
-      <div className="booking__form booking__article--rigth">
-        <TravellerInfo />
-      </div>
-    </article>
-  </>
-
-);
+const Booking = () => {
+  const reservationInformation = {
+    image: 'https://tse4.mm.bing.net/th?id=OIP.h-R1e474we3Kw27WDmz-zwHaEK&pid=Api&P=0',
+    hotelDescription: 'Buena vista',
+    nameHotel: 'Hotel buena vista',
+    city: 'Pereira',
+    country: 'Colombia',
+    checkIn: '2022-12-22',
+    checkOut: '2022-12-25',
+    checkInTime: '06:30:00',
+    checkOutTime: '01:00:00',
+    guestNumber: '2',
+    typeOfRoom: 'Suit',
+    nightsNomber: '3 Night',
+    basePrice: '$ 3000',
+    promoDiscount: '$ 200',
+    taxes: '$ 50',
+  };
+  return (
+    <>
+      <header className="booking__container">
+        <nav className="booking__navigation-bar">
+          <NavigationBar />
+        </nav>
+        <div className="booking__nav">
+          <SliderNav />
+        </div>
+      </header>
+      <article className="booking__article">
+        <div className="booking__summery booking__article--left">
+          <BookingSummery reservationInformation={reservationInformation} />
+        </div>
+        <div className="booking__form booking__article--rigth">
+          <TravellerInfo />
+        </div>
+      </article>
+    </>
+  );
+};
 
 export default Booking;
