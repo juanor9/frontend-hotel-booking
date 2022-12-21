ecc2149a1617cbd6baa344f4dd404f8cb15c92a9
Juan Camilo Orjuela <orjuela9@gmail.com>
2022-12-21 18:26:06 -0500

    Improve registration form with redux and create modal

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,11 +1,14 @@
 /* eslint-disable no-unused-vars */
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import useForm from '../../hooks/useForm';
 import createUser from '../../services/users';
 import './style.css';
+import Modal from '../Modal/Modal';
 
 const RegisterForm = () => {
-  // const { users } = useSelector((state) => state.users);
+  const { users } = useSelector((state) => state.users);
+  const [modal, setModal] = useState(false);
   const { form, handleChange } = useForm({});
   const dispatch = useDispatch();
 
@@ -22,13 +25,14 @@ const RegisterForm = () => {
   const handleClick = () => {
     document.getElementById('register-form__form').reset();
   };
+  useEffect(() => {
+    if (users.length !== 0) setModal(true);
+  }, [users]);
+  const message = 'User has been correctly created. Please check your email to confirm your registration';
   return (
     <section id="register-form" className="register-form">
       <h2 className="register-form__header">Sign up</h2>
-      <article
-        id="social-media-sign-up"
-        className="register-form__section"
-      >
+      <article id="social-media-sign-up" className="register-form__section">
         <p>Sign Up With</p>
         <ul className="register-form__social-media-list">
           <li className="register-form__list-item">
@@ -55,11 +59,7 @@ const RegisterForm = () => {
         <span className="register-form__divider-line" />
       </div>
       <article id="sign-up-form" className="register-form__section">
-        <form
-          action=""
-          onSubmit={handleSubmit}
-          id="register-form__form"
-        >
+        <form action="" onSubmit={handleSubmit} id="register-form__form">
           <label htmlFor="name" className="register-form__input-group">
             Full Name
             <input
@@ -111,13 +111,16 @@ const RegisterForm = () => {
         <span className="register-form__divider-line" />
       </div>
       <article id="login" className="register-form__section">
-        <button
-          type="button"
-          className="register-form__login-button"
-        >
+        <button type="button" className="register-form__login-button">
           Login
         </button>
       </article>
+      {modal === true ? (
+        <Modal
+          modalFunction={setModal}
+          message={message}
+        />
+      ) : null}
     </section>
   );
 };
