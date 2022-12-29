4637345ae2dc00922de178a98db14a2bd986d9f4
Juan Camilo Orjuela <orjuela9@gmail.com>
2022-12-29 13:28:35 -0500

    update exports

--- src/services/auth.js
+++ src/services/auth.js
@@ -2,7 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 
-const login = createAsyncThunk(
+export const login = createAsyncThunk(
   'auth/login',
   async (user) => {
     const options = {
@@ -18,12 +18,9 @@ const login = createAsyncThunk(
     return result;
   },
 );
-
-export default login;
-
-export async function validateUserMailToken(token) {
+export const validateUserMailToken = async (token) => {
   const response = await fetch(`${process.env.REACT_APP_BASE_URL}/auth/local/activate/${token}`);
   const data = response.json();
 
   return data;
-}
+};
