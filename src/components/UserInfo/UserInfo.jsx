ae686456381cf16bf4fb1701dcf5e0f42511d342
Juan Camilo Orjuela <orjuela9@gmail.com>
2022-12-29 19:54:21 -0500

    set basic structure to render profile information

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,63 +1,67 @@
-import PropTypes from 'prop-types';
 import './styles.css';
 
-const UserInfo = ({
-  name, birthday, gender, address, city, zip, email, phone, password,
-}) => (
-  <div className="user-info">
-    <section className="user-info__title">
-      <h2>Profile</h2>
-      <button className="user-info__title-button" type="submit">Edit</button>
-    </section>
-    <section className="user-info__info">
-      <div className="user-info__info-requested">
-        <h4 className="user-info__info-requested-form">Name</h4>
-        <h4 className="user-info__info-requested-form">Birthday</h4>
-        <h4 className="user-info__info-requested-form">Gender</h4>
-        <h4 className="user-info__info-requested-form">Street Address</h4>
-        <h4 className="user-info__info-requested-form">City/State</h4>
-        <h4 className="user-info__info-requested-form">Zip</h4>
-      </div>
-      <div className="user-info__info-user">
-        <p className="user-info__info-user-data">{name}</p>
-        <p className="user-info__info-user-data">{birthday}</p>
-        <p className="user-info__info-user-data">{gender}</p>
-        <p className="user-info__info-user-data">{address}</p>
-        <p className="user-info__info-user-data">{city}</p>
-        <p className="user-info__info-user-data">{zip}</p>
-      </div>
-    </section>
-    <h2 className="user-info__login-title">Login Details</h2>
-    <section className="user-info__login">
-      <div className="user-info__login-info">
-        <h4 className="user-info__login-info-requested">Email Address</h4>
-        <h4 className="user-info__login-info-requested">Phone No</h4>
-        <h4 className="user-info__login-info-requested">Password</h4>
-      </div>
-      <div className="user-info__login-user">
-        <p className="user-info__login-user-data">{email}</p>
-        <p className="user-info__login-user-data">{phone}</p>
-        <p className="user-info__login-user-data">{password}</p>
-      </div>
-      <div className="user-info__login-edit">
-        <button type="submit" className="user-info__login-edit-button">Edit</button>
-        <button type="submit" className="user-info__login-edit-button">Edit</button>
-        <button type="submit" className="user-info__login-edit-button">Edit</button>
-      </div>
-    </section>
-  </div>
-);
+const UserInfo = () => {
+  const user = JSON.parse(localStorage.getItem('user'));
+  return (
+    <div className="user-info">
+      <section className="user-info__title">
+        <h2>Profile</h2>
+        <button className="user-info__title-button" type="submit">Edit</button>
+      </section>
+      <section className="user-info__info">
+        <div className="user-info__info-requested">
+          <h4 className="user-info__info-requested-form">Name</h4>
+          <h4 className="user-info__info-requested-form">Birthday</h4>
+          <h4 className="user-info__info-requested-form">Gender</h4>
+          <h4 className="user-info__info-requested-form">Street Address</h4>
+          <h4 className="user-info__info-requested-form">City/State</h4>
+          <h4 className="user-info__info-requested-form">Zip</h4>
+        </div>
+        <div className="user-info__info-user">
+          {user.name
+            ? <p className="user-info__info-user-data">{user.name}</p>
+            : null}
+          {user.birthday
+            ? <p className="user-info__info-user-data">{user.birthday}</p>
+            : null}
+          {user.gender
+            ? <p className="user-info__info-user-data">{user.gender}</p>
+            : null}
+          {user.address
+            ? <p className="user-info__info-user-data">{user.address}</p>
+            : null}
+          {user.city
+            ? <p className="user-info__info-user-data">{user.city}</p>
+            : null}
+          {user.zipCode
+            ? <p className="user-info__info-user-data">{user.zipCode}</p>
+            : null}
+        </div>
+      </section>
+      <h2 className="user-info__login-title">Login Details</h2>
+      <section className="user-info__login">
+        <div className="user-info__login-info">
+          <h4 className="user-info__login-info-requested">Email Address</h4>
+          <h4 className="user-info__login-info-requested">Phone No</h4>
+          <h4 className="user-info__login-info-requested">Password</h4>
+        </div>
+        <div className="user-info__login-user">
+          {user.email
+            ? <p className="user-info__info-user-data">{user.email}</p>
+            : null}
+          {/* <p className="user-info__login-user-data">{phone}</p> ADD IT TO USER.MODEL */}
 
-UserInfo.propTypes = {
-  name: PropTypes.string.isRequired,
-  birthday: PropTypes.string.isRequired,
-  gender: PropTypes.string.isRequired,
-  address: PropTypes.string.isRequired,
-  city: PropTypes.string.isRequired,
-  zip: PropTypes.number.isRequired,
-  email: PropTypes.string.isRequired,
-  phone: PropTypes.string.isRequired,
-  password: PropTypes.string.isRequired,
+          {/* <p className="user-info__login-user-data">{password}</p>
+           ADD IT TO PROFILE */}
+        </div>
+        <div className="user-info__login-edit">
+          <button type="submit" className="user-info__login-edit-button">Edit</button>
+          <button type="submit" className="user-info__login-edit-button">Edit</button>
+          <button type="submit" className="user-info__login-edit-button">Edit</button>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default UserInfo;
