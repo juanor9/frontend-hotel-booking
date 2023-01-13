325e9c6a539e90eb7877379e03a2ea0c5e9ac954
Juan Camilo Orjuela <orjuela9@gmail.com>
2023-01-12 19:11:15 -0500

    complete styling

--- src/components/HotelGallery/HotelGallery.jsx
+++ src/components/HotelGallery/HotelGallery.jsx
@@ -1,7 +1,8 @@
-/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import './style.css';
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 const HotelGallery = (props) => {
   const {
@@ -10,13 +11,38 @@ const HotelGallery = (props) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   return (
     <section id="hotel-images" className="hotel-images">
-      <div className="image-slider">
-        <figure>
-          <img src={images[currentIndex]} alt="" />
+      <div className="hotel-images__slider">
+        <figure className="hotel-images__figure">
+          <img className="hotel-images__image" src={images[currentIndex]} alt="" />
         </figure>
-        <div className="image-slider__buttons">
-          <button type="button" onClick={() => setCurrentIndex(currentIndex - 1)}>Previous</button>
-          <button type="button" onClick={() => setCurrentIndex(currentIndex + 1)}>Next</button>
+        <div className="hotel-images__buttons">
+          {currentIndex > 0
+            ? (
+              <button
+                type="button"
+                className="hotel-images__button-prev"
+                onClick={
+                () => setCurrentIndex(currentIndex - 1)
+                }
+              >
+                <FontAwesomeIcon icon={faChevronLeft} />
+              </button>
+            )
+            : null}
+          {currentIndex < images.length - 1
+            ? (
+              <button
+                type="button"
+                className="hotel-images__button-next"
+                onClick={
+                () => setCurrentIndex(currentIndex + 1)
+                }
+              >
+                <FontAwesomeIcon icon={faChevronRight} />
+              </button>
+            )
+            : null}
+
         </div>
       </div>
     </section>
