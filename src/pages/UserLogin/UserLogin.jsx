136ac9c890f4d633aff6cb1654a7e21c2ddb4afe
Juan Camilo Orjuela <orjuela9@gmail.com>
2023-01-14 16:14:28 -0500

    create buttons on login form

--- src/pages/UserLogin/UserLogin.jsx
+++ src/pages/UserLogin/UserLogin.jsx
@@ -1,4 +1,5 @@
 import './style.css';
+import { Link } from 'react-router-dom';
 import LoginForm from '../../components/LoginForm/LoginForm';
 import NavigationBar from '../../components/NavigationBar/NavigationBar';
 import SliderNav from '../../components/SliderNav/SliderNav';
@@ -11,6 +12,17 @@ const UserLogin = () => (
     </header>
     <div className="user-login__form-container">
       <LoginForm />
+      <div className="user-login__other">
+        <p>Are you a new user?
+          <Link to="/register">
+            Please register
+          </Link>
+        </p>
+        <p>Did you lost your password?
+          <button type="button">Reset your password</button>
+
+        </p>
+      </div>
     </div>
   </div>
 );
