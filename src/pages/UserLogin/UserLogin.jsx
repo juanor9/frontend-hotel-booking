74cbf039b4dfc1e003e14b3a56fb51c603cb02a7
Juan Camilo Orjuela <orjuela9@gmail.com>
2023-01-15 15:20:44 -0500

    set functions to get user id by email

--- src/pages/UserLogin/UserLogin.jsx
+++ src/pages/UserLogin/UserLogin.jsx
@@ -1,30 +1,90 @@
+/* eslint-disable no-unused-vars */
 import './style.css';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { resetPassword } from '../../services/auth';
 import LoginForm from '../../components/LoginForm/LoginForm';
+import Modal from '../../components/Modal/Modal';
 import NavigationBar from '../../components/NavigationBar/NavigationBar';
 import SliderNav from '../../components/SliderNav/SliderNav';
+import useForm from '../../hooks/useForm';
 
-const UserLogin = () => (
-  <div className="user-login">
-    <header className="user-login__header">
-      <NavigationBar />
-      <SliderNav />
-    </header>
-    <div className="user-login__form-container">
-      <LoginForm />
-      <div className="user-login__other">
-        <p>Are you a new user?
-          <Link to="/register">
-            Please register
-          </Link>
-        </p>
-        <p>Did you lost your password?
-          <button type="button">Reset your password</button>
+const UserLogin = () => {
+  const [modal, setModal] = useState(false);
+  const dispatch = useDispatch();
+  const handleRecoverClick = () => {
+    setModal(true);
+  };
 
-        </p>
+  const { form, handleChange } = useForm({});
+  const handleClick = () => {
+    document.getElementById('reset-password-form').reset();
+  };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      dispatch(resetPassword(form.email));
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
+  return (
+    <div className="user-login">
+      <header className="user-login__header">
+        <NavigationBar />
+        <SliderNav />
+      </header>
+      <div className="user-login__form-container">
+        <LoginForm />
+        <div className="user-login__other">
+          <p>Are you a new user?
+            <Link to="/register">
+              Please register
+            </Link>
+          </p>
+          <p>Did you lost your password?
+            <button
+              type="button"
+              onClick={handleRecoverClick}
+            >Reset your password
+            </button>
+          </p>
+        </div>
       </div>
+      {modal === true ? (
+        <Modal modalFunction={setModal}>
+          <div>
+            <p>To reset your password please provide us with your email.</p>
+            <form onSubmit={handleSubmit} id="reset-password-form">
+              <label
+                htmlFor="email"
+                className="register-form__input-group"
+              >
+                Email Address
+                <input
+                  className="register-form__input"
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Enter email address"
+                  onChange={handleChange}
+                />
+              </label>
+              <button
+                type="submit"
+                className="register-form__create-button"
+                onClick={handleClick}
+              >
+                Send
+              </button>
+            </form>
+          </div>
+        </Modal>
+      ) : null}
     </div>
-  </div>
-);
+  );
+};
 
 export default UserLogin;
--- src/services/auth.js
+++ src/services/auth.js
@@ -19,8 +19,26 @@ export const login = createAsyncThunk(
   },
 );
 export const validateUserMailToken = async (token) => {
-  const response = await fetch(`${process.env.REACT_APP_BASE_URL}/auth/local/activate/${token}`);
+  const response = await fetch(`${BASE_URL}/auth/local/activate/${token}`);
   const data = response.json();
 
   return data;
 };
+
+export const resetPassword = createAsyncThunk(
+  'auth/reset-password',
+  async (email) => {
+    // get user id
+    const getIdOptions = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const res = await fetch(`${BASE_URL}/api/users/search?email=${email}`, getIdOptions);
+    const result = await res.json();
+    const { _id } = result;
+    console.log(_id);
+  },
+);
