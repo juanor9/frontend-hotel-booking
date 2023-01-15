2ad0e91c27487275816fe919ea4bdea68f3d6ca3
Juan Camilo Orjuela <orjuela9@gmail.com>
2023-01-15 11:22:12 -0500

    Feature/hb 84 map (#68)
    * set page changes for user profile with tabs
    
    * update google map for hotels
    
    * set map of each hotel using db coordinates

--- src/pages/UserProfile/UserProfile.jsx
+++ src/pages/UserProfile/UserProfile.jsx
@@ -1,24 +1,52 @@
+import './style.css';
+import { useState, useEffect } from 'react';
 import NavigationBar from '../../components/NavigationBar/NavigationBar';
 import ProfileMenu from '../../components/ProfileMenu/ProfileMenu';
-import UserInfo from '../../components/UserInfo/UserInfo';
 import SliderNav from '../../components/SliderNav/SliderNav';
-import './style.css';
+import UserInfo from '../../components/UserInfo/UserInfo';
+import UserBookings from '../../components/UserBookings/UserBookings';
+import UserPayments from '../../components/UserPayments/UserPayments';
 
-const UserProfile = () => (
-  <div className="profile">
-    <div className="profile__header">
-      <NavigationBar />
-      <SliderNav />
-    </div>
-    <div className="profile__container">
-      <div className="profile__profile-menu">
-        <ProfileMenu />
+const UserProfile = () => {
+  const [navTab, setNavTab] = useState('profile');
+
+  useEffect(() => {
+  }, [navTab]);
+
+  return (
+    <div className="profile">
+      <div className="profile__header">
+        <NavigationBar />
+        <SliderNav />
       </div>
-      <div className="profile__user-info">
-        <UserInfo />
+      <div className="profile__container">
+        <div className="profile__profile-menu">
+          <ProfileMenu navTab={navTab} setNavTab={setNavTab} />
+        </div>
+        {navTab === 'profile'
+          ? (
+            <div className="profile__user-info">
+              <UserInfo />
+            </div>
+          )
+          : null}
+        {navTab === 'bookings'
+          ? (
+            <div className="profile__user-info">
+              <UserBookings />
+            </div>
+          )
+          : null}
+        {navTab === 'payments'
+          ? (
+            <div className="profile__user-info">
+              <UserPayments />
+            </div>
+          )
+          : null}
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default UserProfile;
