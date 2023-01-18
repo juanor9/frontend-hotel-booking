00ed88d0ff23d994c55bc5337ea7954a292dd4b4
Juan Camilo Orjuela <orjuela9@gmail.com>
2023-01-18 11:06:33 -0500

    solve comments

--- src/components/AdminHotels/AdminHotels.jsx
+++ src/components/AdminHotels/AdminHotels.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import './styles.css';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
@@ -12,24 +11,26 @@ const AdminHotels = () => {
     dispatch(getHotels());
   }, []);
   return (
-    <section className="hotelsManagment__list">
-      <h2>Hotels Managment</h2>
-      {
-    hotels.map((hotel) => (
-      <HotelCardAdmin
-        imageProfile={hotel.imageProfile}
-        name={hotel.name}
-        about={hotel.about}
-        city={hotel.city}
-        pricePerNight={hotel.pricePerNight}
-        offerPrice={hotel.offerPrice}
-        feature1={hotel.feature1}
-        feature2={hotel.feature2}
-        id={hotel._id}
-        key={hotel._id}
-      />
-    ))
+    <section className="admin-hotels">
+      <h2 className="admin-hotels__header">Hotels Managment</h2>
+      <div className="hotelsManagment__list">
+        {
+        hotels.map((hotel) => (
+          <HotelCardAdmin
+            imageProfile={hotel.imageProfile}
+            name={hotel.name}
+            about={hotel.about}
+            city={hotel.city}
+            pricePerNight={hotel.pricePerNight}
+            offerPrice={hotel.offerPrice}
+            feature1={hotel.feature1}
+            feature2={hotel.feature2}
+            id={hotel._id}
+            key={hotel._id}
+          />
+        ))
   }
+      </div>
     </section>
   );
 };
