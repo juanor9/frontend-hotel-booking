12d9ca54f12b0578c52ad029389a356f0f56ead2
GDiana <dcquinche@gmail.com>
2023-01-19 12:18:53 -0500

    add profile button in success

--- src/components/SuccessfulPayment/SuccessfulPayment.jsx
+++ src/components/SuccessfulPayment/SuccessfulPayment.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import van from '../../assets/van.jpg';
 import './styles.css';
 
@@ -5,7 +6,8 @@ const SuccessfulPayment = () => (
   <div className="success-component">
     <img className="success-component__img" alt="van" src={van} />
     <h1 className="success-component__title">Payment Successful! Get Ready For Comfortable Stay.</h1>
-    <p className="success-component__text">Thank You For Your Payment. We Have Received Your Payment Successfully. You Will Get An Email Invoice Soon!</p>
+    <p className="success-component__text">Thank You For Your Payment. We Have Received Your Payment Successfully. We hope you enjoy your stay in our hotel!</p>
+    <Link to="/profile"><button className="success-component__button" type="submit">Profile</button></Link>
   </div>
 );
 
