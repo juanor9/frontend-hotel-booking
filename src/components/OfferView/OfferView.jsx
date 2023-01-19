d53207cfba90afce6635256e87f7a85e5189655c
Julian Gomez <julgomez14@gmail.com>
2023-01-19 15:38:30 -0500

    home page improvements

--- src/components/OfferView/OfferView.jsx
+++ src/components/OfferView/OfferView.jsx
@@ -1,42 +1,63 @@
 import './styles.css';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getHotels } from '../../features/hotels/hotelsSlice';
 import OfferCard from '../OfferCard/OfferCard';
-import OfferFoodImage from '../../assets/offerFood.jpg';
-import data from '../../assets/offer.json';
 
-const OfferView = () => (
-  <section className="offerview__card">
-    <div className="offerview__title">
-      <h2>top offerts</h2>
-    </div>
-    <div className="offerview__component">
-      <article>
-        {
-          window.innerWidth > 768
-            ? (
-              <div className="card__descktopHome">{
-                data.map((e) => (
-                  <OfferCard
-                    key={e.id}
-                    title={e.title}
-                    text={e.text}
-                    image={OfferFoodImage}
-                  />
-                ))
-              }
-              </div>
-            ) : (
-              <div>
-                <OfferCard
-                  title="Hotel Booking"
-                  text="Avail hot deals on hotel"
-                  image={OfferFoodImage}
-                />
-              </div>
-            )
-        }
-      </article>
-    </div>
-  </section>
-);
+const OfferView = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getHotels());
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [window.innerWidth]);
+
+  const { hotels } = useSelector((state) => state.hotels);
+  const hotelsToRender = hotels.slice(0, 5);
+  const renderOne = hotels.slice(0, 1);
+  return (
+    <section className="offerview__card">
+      <div className="offerview__title">
+        <h2>top offerts</h2>
+      </div>
+      <div className="offerview__component">
+        <article>
+          {
+            width > 800
+              ? (
+                <div className="card__descktopHome">{
+                  hotelsToRender.map((hotel) => (
+                    <OfferCard
+                      key={hotel._id}
+                      title={hotel.name}
+                      text={hotel.country}
+                      image={hotel.imageProfile}
+                    />
+                  ))
+                }
+                </div>
+              ) : (
+                <div>{
+                  renderOne.map((hotel) => (
+                    <OfferCard
+                      key={hotel._id}
+                      title={hotel.name}
+                      text={hotel.country}
+                      image={hotel.imageProfile}
+                    />
+                  ))
+                }
+                </div>
+              )
+          }
+        </article>
+      </div>
+    </section>
+  );
+};
 
 export default OfferView;
